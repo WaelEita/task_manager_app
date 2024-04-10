ce8b901447e4d6e1c0deb0c141f7de916ddc5d4c
wael <waeletta4@gmail.com>
2024-04-10 22:36:35 +0300

    Create the font styles file

---
+++ lib/utils/font_styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const TextStyle boldTextStyle26 = TextStyle(
+    fontSize: 26,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyle18 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+}
