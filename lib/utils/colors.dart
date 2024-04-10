378481b32f36314c8d407c4fed56450f1d9d38f3
wael <waeletta4@gmail.com>
2024-04-10 22:36:53 +0300

    Create colors file

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF212832);
+const kSecondary = Color(0xFFFED36A);
+const black = Color(0xFF000000);
+const white = Color(0xFFFFFFFF);
+const softGrey = Color(0xFF8CAAB9);
+const darkGrey = Color(0xFF455A64);
