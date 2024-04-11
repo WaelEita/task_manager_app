135113049efbfcc14f6e8f1e871704a3e230d9c0
wael <waeletta4@gmail.com>
2024-04-12 00:03:12 +0300

    Edit API error handler

--- lib/utils/api_error_handler.dart
+++ lib/utils/api_error_handler.dart
@@ -1,55 +1,56 @@
 import 'package:dio/dio.dart';
 
 abstract class Failures {
-  final String errorMassage;
+  final String errorMessage;
 
-  Failures({required this.errorMassage});
+  Failures({required this.errorMessage});
 }
 
 class ServerFailures extends Failures {
-  ServerFailures({required super.errorMassage});
+  ServerFailures({required super.errorMessage});
 
   factory ServerFailures.ServerFailures(DioException dioException) {
     switch (dioException.type) {
       case DioExceptionType.connectionTimeout:
-        return ServerFailures(errorMassage: 'Connection timeout with api');
+        return ServerFailures(errorMessage: 'Connection timeout with api');
 
       case DioExceptionType.sendTimeout:
-        return ServerFailures(errorMassage: 'Send timeout with api');
+        return ServerFailures(errorMessage: 'Send timeout with api');
 
       case DioExceptionType.receiveTimeout:
-        return ServerFailures(errorMassage: 'Receive timeout with api');
+        return ServerFailures(errorMessage: 'Receive timeout with api');
 
       case DioExceptionType.badCertificate:
         return ServerFailures.fromResponse(
             response: dioException.response!.data,
             statusCode: dioException.response!.statusCode!);
       case DioExceptionType.badResponse:
-        return ServerFailures(errorMassage: 'Error pls try again');
+        return ServerFailures(errorMessage: 'Error pls try again');
 
       case DioExceptionType.cancel:
-        return ServerFailures(errorMassage: 'The requise canceld');
+        return ServerFailures(errorMessage: 'The requise canceld');
 
       case DioExceptionType.connectionError:
-        return ServerFailures(errorMassage: 'Bad connection');
+        return ServerFailures(errorMessage: 'Bad connection');
 
       case DioExceptionType.unknown:
-        return ServerFailures(errorMassage: 'There is no internet');
+        return ServerFailures(errorMessage: 'There is no internet');
     }
   }
 
   factory ServerFailures.fromResponse(
       {required dynamic response, required int statusCode}) {
-    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
-      return ServerFailures(errorMassage: response['error']['massage']);
+    if (statusCode == 401) {
+      return ServerFailures(errorMessage: 'Token expired. Please refresh.');
+    } else if (statusCode == 400 || statusCode == 403) {
+      return ServerFailures(errorMessage: response['error']['message']);
     } else if (statusCode == 404) {
-      return ServerFailures(errorMassage: "There is an error 404");
+      return ServerFailures(errorMessage: "The session has timed out");
     } else if (statusCode == 500) {
       return ServerFailures(
-          errorMassage: "Internal server error, Please try again later");
+          errorMessage: "Internal server error, Please try again later");
     } else {
-      return ServerFailures(
-          errorMassage: "There is an error please try again later");
+      return ServerFailures(errorMessage: "Server error, please try again later");
     }
   }
 }
